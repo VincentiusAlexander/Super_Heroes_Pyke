81843b8a895481fbf94e2e15cf99cd0d99d08b59
Vincentius Alexander <vincentknights7@gmail.com>
2021-11-08 23:17:59 +0700

    test

---
+++ driver_simple.py
@@ -0,0 +1,27 @@
+import contextlib
+import sys
+
+from pyke import knowledge_engine
+from pyke import krb_traceback
+
+def test_questions():
+
+    engine.reset()      # Allows us to run tests multiple times.
+
+    engine.activate('sp_questions') #STUDENTS: you will need to edit the name of your rule file here
+
+    print("doing proof")
+    
+    try:
+        with engine.prove_goal('sp_questions.is($answer)') as gen: #STUDENTS: you will need to edit this line
+            for vars, plan in gen:
+                print("It is: %s" % (vars['answer'])) #STUDENTS: you will need to edit this line
+
+    except Exception:
+        # This converts stack frames of generated python functions back to the
+        # .krb file.
+        krb_traceback.print_exc()
+        sys.exit(1)
+
+    print()
+    print("done")
