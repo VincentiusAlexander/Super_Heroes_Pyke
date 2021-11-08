4df300fd2620a033a4ede830d75aaaff2e8b8e08
Vincentius Alexander <vincentknights7@gmail.com>
2021-11-09 00:04:18 +0700

    test

--- driver_simple.py
+++ driver_simple.py
@@ -6,18 +6,42 @@ from pyke import krb_traceback
 
 engine = knowledge_engine.engine(__file__)
 
-def test_questions():
+
+def bc_test():
 
     engine.reset()      # Allows us to run tests multiple times.
 
-    engine.activate('sp_questions') #STUDENTS: you will need to edit the name of your rule file here
+    engine.activate('bc_simple_rules') #STUDENTS: you will need to edit the name of your rule file here
 
     print("doing proof")
     
     try:
-        with engine.prove_goal('sp_questions.it_is($answer)') as gen: #STUDENTS: you will need to edit this line
+        with engine.prove_goal('bc_simple_rules.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
             for vars, plan in gen:
-                print("It is: %s" % (vars['answer'])) #STUDENTS: you will need to edit this line
+                print("You should bring: %s" % (vars['bring'])) #STUDENTS: you will need to edit this line
+    except Exception:
+        # This converts stack frames of generated python functions back to the
+        # .krb file.
+        krb_traceback.print_exc()
+        sys.exit(1)
+
+    print()
+    print("done")
+    #engine.print_stats()
+
+    
+def bc_test_questions():
+
+    engine.reset()      # Allows us to run tests multiple times.
+
+    engine.activate('bc_simple_rules_questions') #STUDENTS: you will need to edit the name of your rule file here
+
+    print("doing proof")
+    
+    try:
+        with engine.prove_goal('bc_simple_rules_questions.what_to_bring($bring)') as gen: #STUDENTS: you will need to edit this line
+            for vars, plan in gen:
+                print("You should bring: %s" % (vars['bring'])) #STUDENTS: you will need to edit this line
 
     except Exception:
         # This converts stack frames of generated python functions back to the
